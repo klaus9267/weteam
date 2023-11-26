b12c36982cd001a1e651d799c492bfadee44e311
klaus9267 <klaus9267@gmail.com>
2023-11-27 00:43:26 +0900

    repo 이전

--- src/main/java/weteam/backend/member/repository/MemberCustomRepositoryImpl.java
+++ src/main/java/weteam/backend/member/repository/MemberCustomRepositoryImpl.java
@@ -1,15 +1,9 @@
 package weteam.backend.member.repository;
 
-import com.querydsl.jpa.JPAExpressions;
-import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-import weteam.backend.hash_tag.domain.MemberHashtag;
-import weteam.backend.hash_tag.domain.QMemberHashtag;
 import weteam.backend.member.domain.Member;
-
-import java.util.stream.Collectors;
 
 import static weteam.backend.hash_tag.domain.QHashtag.hashtag;
 import static weteam.backend.hash_tag.domain.QMemberHashtag.memberHashtag;
