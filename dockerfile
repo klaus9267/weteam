50f58c0edb6a74923ef25d697092fef220be17d1
Kim minho <klaus9267@gmail.com>
2024-01-09 17:09:52 +0900

    Feat: firebase 환경변수 추가

--- dockerfile
+++ dockerfile
@@ -3,11 +3,9 @@ FROM openjdk:17.0.2-jdk
 ENV APP_HOME=/apps
 
 ARG JAR_FILE_PATH=build/libs/backend-0.0.1-SNAPSHOT.jar
-ARG FIREBASE_JSON_PATH=./src/main/resources/firebase.json
 
 WORKDIR $APP_HOME
 
-COPY $FIREBASE_JSON_PATH .
 COPY $JAR_FILE_PATH app.jar
 
 EXPOSE 8080
--- src/main/java/weteam/backend/application/auth/FirebaseInitializer.java
+++ src/main/java/weteam/backend/application/auth/FirebaseInitializer.java
@@ -1,22 +1,88 @@
 package weteam.backend.application.auth;
 
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class FirebaseInitializer {
+    @Value("${firebase.type}")
+    private String type;
+
+    @Value("${firebase.project_id}")
+    private String projectId;
+
+    @Value("${firebase.private_key_id}")
+    private String privateKeyId;
+
+    @Value("${firebase.private_key}")
+    private String privateKey;
+
+    @Value("${firebase.client_email}")
+    private String clientEmail;
+
+    @Value("${firebase.client_id}")
+    private String clientId;
+
+    @Value("${firebase.auth_uri}")
+    private String authUri;
+
+    @Value("${firebase.token_uri}")
+    private String tokenUri;
+
+    @Value("${firebase.auth_provider_x509_cert_url}")
+    private String authProviderX509CertUrl;
+
+    @Value("${firebase.client_x509_cert_url}")
+    private String clientX509CertUrl;
+
+    @Value("${firebase.universe_domain}")
+    private String universe_domain;
+
     @Bean
     public FirebaseAuth getFirebaseAuth() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("./src/main/resources/firebase.json");
-        FirebaseOptions options = FirebaseOptions.builder().setCredentials(GoogleCredentials.fromStream(serviceAccount)).build();
-        FirebaseApp.initializeApp(options);
-        return FirebaseAuth.getInstance(FirebaseApp.getInstance());
+        GoogleCredentials credentials = ServiceAccountCredentials.fromStream(
+                new ByteArrayInputStream(createFirebaseConfigString().getBytes(StandardCharsets.UTF_8))
+        );
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                                                 .setCredentials(credentials)
+                                                 .setProjectId(projectId)
+                                                 .build();
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(options);
+        }
+
+        return FirebaseAuth.getInstance();
+    }
+
+    private String createFirebaseConfigString() {
+        return "{"
+               + "\"type\": \"service_account\","
+               + "\"project_id\": \"" + projectId + "\","
+               + "\"private_key_id\": \"" + privateKeyId +"\","
+               + "\"private_key\": \"" + privateKey + "\","
+               + "\"client_email\": \"" + clientEmail + "\","
+               + "\"client_id\": \"" + clientId + "\","
+               + "\"auth_uri\": \"https://accounts.google.com/o/oauth2/auth\","
+               + "\"token_uri\": \"https://oauth2.googleapis.com/token\","
+               + "\"auth_provider_x509_cert_url\": \"https://www.googleapis.com/oauth2/v1/certs\","
+               + "\"client_x509_cert_url\": \"" + clientX509CertUrl + "\","
+               + "\"universe_domain\": \"" + universe_domain + "\""
+               + "}";
     }
 }
