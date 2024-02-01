2d1fdd2d254269fcddbc1a2cb1389e23253f787d
Kim minho <klaus9267@gmail.com>
2024-02-01 22:35:38 +0900

    Feat: bulk insert 구현 중

--- src/main/java/weteam/backend/domain/bulk/BulkController.java
+++ src/main/java/weteam/backend/domain/bulk/BulkController.java
@@ -1,4 +1,32 @@
-//package weteam.backend.domain.bulk;
-//
-//public class BulkController {
-//}
+package weteam.backend.domain.bulk;
+
+import io.swagger.v3.oas.annotations.Parameter;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import weteam.backend.domain.bulk.repository.ProjectBulk;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/bulk")
+@RequiredArgsConstructor
+public class BulkController {
+    private final ProjectBulk projectBulk;
+
+    @PostMapping("/projects")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void bulkProject(
+            @Parameter(example = "100", description = "size^2 만큼 데이터가 저장됩니다. 100 입력시 10,000개 저장, 최대값 100")
+            @Valid @Max(100)
+            @RequestParam("size") final Integer size,
+            @Parameter(example = "2023-10-01T00:00:00", description = "랜덤 날짜 시작일")
+            @RequestParam("start") final LocalDateTime start,
+            @Parameter(example = "2024-02-01T00:00:00", description = "랜덤 날짜 종료일")
+            @RequestParam("end") final LocalDateTime end
+    ) {
+        projectBulk.bulkInsert(size, start, end);
+    }
+}
