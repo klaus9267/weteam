e60ecf73aab22b042c1961f8c22034180c4bb4f1
Kim minho <klaus9267@gmail.com>
2024-02-08 15:52:06 +0900

    Feat: 요청에 대한 상세 log 추가

--- src/main/java/weteam/backend/application/auth/FirebaseInitializer.java
+++ src/main/java/weteam/backend/application/auth/FirebaseInitializer.java
@@ -1,36 +1,36 @@
 package weteam.backend.application.auth;
 
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseToken;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
-import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 public class FirebaseInitializer {
-    @Bean
-    public FirebaseAuth getFirebaseAuth() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("./firebase.json");
-
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setDatabaseUrl("https://weteam-1968a-default-rtdb.firebaseio.com")
-                .build();
-
-        FirebaseApp.initializeApp(options);
-
-        return FirebaseAuth.getInstance();
-    }
+  private final String databaseUrl;
+  
+  public FirebaseInitializer(@Value("${firebase.database-url}") final String databaseUrl) {
+    this.databaseUrl = databaseUrl;
+  }
+  
+  @Bean
+  
+  public FirebaseAuth getFirebaseAuth() throws IOException {
+    FileInputStream serviceAccount = new FileInputStream("./firebase.json");
+    
+    FirebaseOptions options = new FirebaseOptions.Builder()
+        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+        .setDatabaseUrl(databaseUrl)
+        .build();
+    
+    FirebaseApp.initializeApp(options);
+    
+    return FirebaseAuth.getInstance();
+  }
 }
