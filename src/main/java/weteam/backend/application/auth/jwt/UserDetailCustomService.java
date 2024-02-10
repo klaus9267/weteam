a94045dab978a3c1d90dde378fcf1dd0f78fdb23
klaus9267 <klaus9267@gmail.com>
2024-02-10 16:20:33 +0900

    Feat: git ignore 설정

--- src/main/java/weteam/backend/application/auth/jwt/UserDetailCustomService.java
+++ src/main/java/weteam/backend/application/auth/jwt/UserDetailCustomService.java
@@ -13,21 +13,21 @@ import weteam.backend.domain.user.entity.UserRole;
 @AllArgsConstructor
 @Service
 public class UserDetailCustomService {
-  private final UserRepository userRepository;
-  
-  @Transactional
-  public UserDto loadUser(FirebaseToken token) {
-    User user = registerUser(token);
-    return UserDto.from(user);
-  }
-  
-  private User registerUser(FirebaseToken token) {
-    return userRepository.findByUid(token.getUid()).orElseGet(() -> userRepository.save(
-        User.builder()
-            .uid(token.getUid())
-            .email(token.getEmail())
-            .username(token.getName())
-            .role(UserRole.USER)
-            .build()));
-  }
+    private final UserRepository userRepository;
+
+    @Transactional
+    public UserDto loadUser(FirebaseToken token) {
+        User user = registerUser(token);
+        return UserDto.from(user);
+    }
+
+    private User registerUser(FirebaseToken token) {
+        return userRepository.findByUid(token.getUid()).orElseGet(() -> userRepository.save(
+                User.builder()
+                    .uid(token.getUid())
+                    .email(token.getEmail())
+                    .username(token.getName())
+                    .role(UserRole.USER)
+                    .build()));
+    }
 }
