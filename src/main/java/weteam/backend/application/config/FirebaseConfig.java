d98149b81a53e6c991888d3a5b9fcec0b5757d60
Kim minho <klaus9267@gmail.com>
2024-02-16 05:34:00 +0900

    Feat: auth 파일 정리

---
+++ src/main/java/weteam/backend/application/config/FirebaseConfig.java
@@ -0,0 +1,36 @@
+package weteam.backend.application.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+  private final String databaseUrl;
+  
+  public FirebaseConfig(@Value("${firebase.database-url}") final String databaseUrl) {
+    this.databaseUrl = databaseUrl;
+  }
+  
+  @Bean
+  
+  public FirebaseAuth getFirebaseAuth() throws IOException {
+    FileInputStream serviceAccount = new FileInputStream("./firebase.json");
+    
+    FirebaseOptions options = new FirebaseOptions.Builder()
+        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+        .setDatabaseUrl(databaseUrl)
+        .build();
+    
+    FirebaseApp.initializeApp(options);
+    
+    return FirebaseAuth.getInstance();
+  }
+}
