3e7167af916efb21ab63ad3eafb189473a4e7099
Kim minho <klaus9267@gmail.com>
2024-02-16 19:53:24 +0900

    Feat: FCM config 작성

--- src/main/java/weteam/backend/application/config/FirebaseConfig.java
+++ src/main/java/weteam/backend/application/config/FirebaseConfig.java
@@ -4,12 +4,15 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
@@ -20,7 +23,6 @@ public class FirebaseConfig {
   }
   
   @Bean
-  
   public FirebaseAuth getFirebaseAuth() throws IOException {
     FileInputStream serviceAccount = new FileInputStream("./firebase.json");
     
@@ -32,5 +34,24 @@ public class FirebaseConfig {
     FirebaseApp.initializeApp(options);
     
     return FirebaseAuth.getInstance();
+  }
+  
+  @Bean
+  public FirebaseMessaging firebaseMessaging() throws IOException {
+    ClassPathResource resource = new ClassPathResource("./firebase.json");
+    InputStream refreshToken = resource.getInputStream();
+    FirebaseApp firebaseApp = FirebaseApp.getApps()
+                                         .stream()
+                                         .filter(app -> FirebaseApp.DEFAULT_APP_NAME.equals(app.getName()))
+                                         .findFirst()
+                                         .orElseGet(() -> {
+                                           try {
+                                             final FirebaseOptions options = FirebaseOptions.builder().setCredentials(GoogleCredentials.fromStream(refreshToken)).build();
+                                             return FirebaseApp.initializeApp(options);
+                                           } catch (IOException e) {
+                                             throw new RuntimeException(e);
+                                           }
+                                         });
+    return FirebaseMessaging.getInstance(firebaseApp);
   }
 }
