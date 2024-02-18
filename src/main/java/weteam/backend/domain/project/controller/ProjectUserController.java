9c3981b8ced3f10b3758f641b274bc747511e7b1
Kim minho <klaus9267@gmail.com>
2024-02-18 16:49:42 +0900

    Feat: 팀원 초대 api

--- src/main/java/weteam/backend/domain/project/controller/ProjectUserController.java
+++ src/main/java/weteam/backend/domain/project/controller/ProjectUserController.java
@@ -10,6 +10,7 @@ import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import weteam.backend.domain.common.swagger.SwaggerCreated;
 import weteam.backend.domain.common.swagger.SwaggerNoContent;
 import weteam.backend.domain.common.swagger.SwaggerOK;
 import weteam.backend.domain.project.dto.ProjectUserDto;
@@ -23,41 +24,50 @@ import java.util.List;
 @RequiredArgsConstructor
 @Tag(name = "PROJECT_USER")
 public class ProjectUserController {
-    private final ProjectUserService projectUserService;
-
-    @GetMapping("{projectId}")
-    @SwaggerOK(summary = "팀원 목록 조회")
-    public ResponseEntity<List<ProjectUserDto>> findProjectMemberList(@PathVariable("projectId") final Long projectId) {
-        return ResponseEntity.ok(projectUserService.findProjectUserListByProjectId(projectId));
-    }
-
-    @PatchMapping
-    @Operation(summary = "담당 역할 변경", description = "응답 없음")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void updateProjectRole(@ParameterObject @Valid UpdateProjectRoleParam updateProjectRoleParam) {
-        projectUserService.updateProjectRole(updateProjectRoleParam);
-    }
-
-    @PatchMapping("{projectId}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @SwaggerNoContent(summary = "초대 수락", description = "응답 없음")
-    public void acceptInvite(@PathVariable("projectId") final Long projectId) {
-        projectUserService.acceptInvite(projectId);
-    }
-
-    @DeleteMapping
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @SwaggerNoContent(summary = "팀원 강퇴", description = "응답 없음")
-    public void kickUser(
-            @Parameter(description = "userId가 아닌 projectUserId")
-            @NotNull @RequestParam("projectUserIdList") final List<Long> projectUserIdList) {
-        projectUserService.kickUsers(projectUserIdList);
-    }
-
-    @DeleteMapping("{projectId}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @SwaggerNoContent(summary = "팀플 탈퇴", description = "응답 없음")
-    public void exitProject(@PathVariable("projectId") final Long projectId) {
-        projectUserService.exitProject(projectId);
-    }
+  private final ProjectUserService projectUserService;
+  
+  @PostMapping("{projectId}/{userId}")
+  @SwaggerCreated(summary = "팀원 초대")
+  public void inviteProject(
+      @PathVariable("projectId") final Long projectId,
+      @PathVariable("userId") final Long userId
+  ) {
+    projectUserService.inviteProject(projectId, userId);
+  }
+  
+  @GetMapping("{projectId}")
+  @SwaggerOK(summary = "팀원 목록 조회")
+  public ResponseEntity<List<ProjectUserDto>> findProjectMemberList(@PathVariable("projectId") final Long projectId) {
+    return ResponseEntity.ok(projectUserService.findProjectUserListByProjectId(projectId));
+  }
+  
+  @PatchMapping
+  @Operation(summary = "담당 역할 변경", description = "응답 없음")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void updateProjectRole(@ParameterObject @Valid UpdateProjectRoleParam updateProjectRoleParam) {
+    projectUserService.updateProjectRole(updateProjectRoleParam);
+  }
+  
+  @PatchMapping("{projectId}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  @SwaggerNoContent(summary = "초대 수락", description = "응답 없음")
+  public void acceptInvite(@PathVariable("projectId") final Long projectId) {
+    projectUserService.acceptInvite(projectId);
+  }
+  
+  @DeleteMapping
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  @SwaggerNoContent(summary = "팀원 강퇴", description = "응답 없음")
+  public void kickUser(
+      @Parameter(description = "userId가 아닌 projectUserId")
+      @NotNull @RequestParam("projectUserIdList") final List<Long> projectUserIdList) {
+    projectUserService.kickUsers(projectUserIdList);
+  }
+  
+  @DeleteMapping("{projectId}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  @SwaggerNoContent(summary = "팀플 탈퇴", description = "응답 없음")
+  public void exitProject(@PathVariable("projectId") final Long projectId) {
+    projectUserService.exitProject(projectId);
+  }
 }
