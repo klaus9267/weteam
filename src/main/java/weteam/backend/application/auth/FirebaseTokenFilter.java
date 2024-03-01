0e3a70dbae471f0ff628884cf620d6eac274f4b7
Kim minho <klaus9267@gmail.com>
2024-03-01 18:26:52 +0900

    Feat: 오류 검출을 위한 요청에 대한 로그 수정

--- src/main/java/weteam/backend/application/auth/FirebaseTokenFilter.java
+++ src/main/java/weteam/backend/application/auth/FirebaseTokenFilter.java
@@ -49,7 +49,7 @@ public class FirebaseTokenFilter extends OncePerRequestFilter {
     }
     
     UserDto user = userDetailCustomService.loadUser(decodedToken);
-    log.info("------------------------------ login : " + request.getRemoteUser() + " ------------------------------");
+    log.info("------------------------------ login : " + user.toString() + " ------------------------------");
     UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user, null, List.of(new SimpleGrantedAuthority(UserRole.USER.getKey())));
     SecurityContextHolder.getContext().setAuthentication(authentication);
     
