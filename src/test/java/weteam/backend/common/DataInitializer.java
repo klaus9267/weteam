fe13c0e76aaa6411986c3e649ca4c160ac59149b
Kim minho <klaus9267@gmail.com>
2024-05-09 01:02:18 +0900

    Test: test 준비

---
+++ src/test/java/weteam/backend/common/DataInitializer.java
@@ -0,0 +1,36 @@
+package weteam.backend.common;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import weteam.backend.domain.user.UserRepository;
+import weteam.backend.domain.user.entity.User;
+import weteam.backend.domain.user.entity.UserRole;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@TestConfiguration
+@RequiredArgsConstructor
+public class DataInitializer {
+  private final UserRepository userRepository;
+  List<User> users = new ArrayList<>();
+
+  @PostConstruct
+  public void setContext() {
+    final UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(users.get(0), null, List.of(new SimpleGrantedAuthority(UserRole.USER.getKey())));
+    SecurityContextHolder.getContext().setAuthentication(authentication);
+  }
+
+  private void saveUsers() {
+    List<User> userList = new ArrayList<>();
+    for (int i = 0; i < 100; i++) {
+      User user = new User(null, "user" + i, "password", "name" + i, "nickname" + i, "email" + i + "@email.com", UserRole.USER, null, null, null, null);
+      userList.add(user);
+    }
+    users = userRepository.saveAll(userList);
+  }
+}
--- src/test/java/weteam/backend/domain/user/UserControllerTest.java
+++ src/test/java/weteam/backend/domain/user/UserControllerTest.java
@@ -3,13 +3,17 @@ package weteam.backend.domain.user;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 class UserControllerTest {
   private final String END_POINT = "/api/users";
 
