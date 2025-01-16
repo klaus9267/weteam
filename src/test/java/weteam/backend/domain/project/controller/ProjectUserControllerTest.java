e7dbbf86b9c9493f6b5430d67675fb6865cb46b5
Kim minho <klaus9267@gmail.com>
2025-01-16 11:08:42 +0900

    Test: projectUser 통합 테스트 수정

--- src/test/java/weteam/backend/domain/project/controller/ProjectUserControllerTest.java
+++ src/test/java/weteam/backend/domain/project/controller/ProjectUserControllerTest.java
@@ -1,17 +1,21 @@
 package weteam.backend.domain.project.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import weteam.backend.common.BaseIntegrationTest;
-import weteam.backend.common.DataInitializer;
-import weteam.backend.domain.common.pagination.param.ProjectPaginationParam;
-import weteam.backend.domain.project.entity.Project;
-import weteam.backend.domain.project_user.entity.ProjectUser;
+import weteam.backend.application.firebase.FirebaseUtil;
+import weteam.backend.common.IntegrationTest;
 import weteam.backend.domain.project.ProjectRepository;
+import weteam.backend.domain.project.entity.Project;
 import weteam.backend.domain.project_user.ProjectUserRepository;
-import weteam.backend.domain.user.entity.User;
+import weteam.backend.domain.project_user.entity.ProjectUser;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -20,54 +24,74 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-class ProjectUserControllerTest extends BaseIntegrationTest {
-  private final String END_POINT = "/api/project-users";
-
+@IntegrationTest
+@Import(ProjectFixture.class)
+class ProjectUserControllerTest {
+  private String token;
+  String END_POINT = "/api/project-users";
+  @Autowired
+  MockMvc mockMvc;
+  @Autowired
+  protected ObjectMapper mapper = new ObjectMapper();
+  @Autowired
+  FirebaseUtil firebaseUtil;
   @Autowired
   ProjectRepository projectRepository;
   @Autowired
   ProjectUserRepository projectUserRepository;
+  @Autowired
+  ProjectFixture projectFixture;
+
+  @BeforeEach
+  void setUp() {
+    token = "Bearer " + firebaseUtil.createIdToken();
+    this.mapper.registerModule(new JavaTimeModule());
+  }
 
   @Nested
-  class 성공 {
+  @DisplayName("성공")
+  class Success {
     @Test
-    void 팀원_초대용_hashedId_조회() throws Exception {
-      Project project = testRepository.saveProject();
+    @DisplayName("팀원_초대용_hashedId_조회")
+    void readHashId() throws Exception {
+      Project project = projectRepository.findById(1L).orElseThrow(NoSuchElementException::new);
 
-      mockMvc.perform(get(END_POINT + "/" + project.getId() + "/invite")
-              .header("Authorization", idToken)
-          ).andExpect(status().isOk())
-          .andExpect(content().string(project.getHashedId()));
+      ResultActions actions = mockMvc.perform(get(END_POINT + "/" + project.getId() + "/invite")
+          .header("Authorization", token)
+      ).andExpect(status().isOk());
+
+      actions.andExpect(content().string(project.getHashedId()));
     }
 
     @Test
-    void 팀원_목록_조회() throws Exception {
-      Project project = findJoinedProject();
+    @DisplayName("팀원_목록_조회")
+    void readProjectUsers() throws Exception {
+      Project project = projectFixture.joinProject(2L, 1L);
 
       ResultActions actions = mockMvc.perform(get(END_POINT + "/" + project.getId())
-          .header("Authorization", idToken)
+          .header("Authorization", token)
       ).andExpect(status().isOk());
 
       for (int i = 0; i < project.getProjectUserList().size(); i++) {
         ProjectUser projectUser = project.getProjectUserList().get(i);
         actions.andExpect(jsonPath("$[" + i + "].id").value(projectUser.getId()));
         actions.andExpect(jsonPath("$[" + i + "].role").value(projectUser.getRole()));
-        actions.andExpect(jsonPath("$[" + i + "].enable").value(projectUser.isEnable()));
       }
     }
 
     @Test
-    void 담당_역할_변경() throws Exception {
-      Project project = findJoinedProject();
+    @DisplayName("담당_역할_변경")
+    void updateRole() throws Exception {
+      Project project = projectRepository.findById(1L).orElseThrow(NoSuchElementException::new);
       String role = "test tole";
 
       mockMvc.perform(patch(END_POINT)
-          .header("Authorization", idToken)
-          .param("projectId", String.valueOf(project.getId()))
+          .header("Authorization", token)
+          .param("projectId", String.valueOf(1))
           .param("role", role)
       ).andExpect(status().isNoContent());
 
-      ProjectUser projectUser = findProjectUserById(project.getProjectUserList().get(0).getId());
+      ProjectUser projectUser = projectUserRepository.findByProjectIdAndUserId(1L, 1L).orElseThrow(NoSuchElementException::new);
       assertThat(projectUser).extracting(
           ProjectUser::getId,
           ProjectUser::getProject,
@@ -75,67 +99,118 @@ class ProjectUserControllerTest extends BaseIntegrationTest {
       ).containsExactly(
           project.getProjectUserList().get(0).getId(),
           project,
-          project.getProjectUserList().get(0).getRole()
+          role
       );
     }
 
     @Test
-    void 초대_수락() throws Exception {
-      User user = DataInitializer.testUser;
-      Project project = projectRepository.findAll().stream()
-          .filter(projectEl -> projectEl.getProjectUserList().stream()
-              .noneMatch(projectUser -> projectUser.getUser().getId().equals(user.getId()))
-          ).findFirst().orElseThrow(NoSuchElementException::new);
-
+    @DisplayName("초대_수락")
+    void acceptInvite() throws Exception {
+      Project project = projectRepository.findById(2L).orElseThrow(NoSuchElementException::new);
 
       mockMvc.perform(patch(END_POINT + "/" + project.getHashedId())
-          .header("Authorization", idToken)
+          .header("Authorization", token)
       ).andExpect(status().isNoContent());
 
       Project foundProject = projectRepository.findById(project.getId()).orElseThrow(NoSuchElementException::new);
-      assertThat(foundProject.getProjectUserList().get(0)).isNotNull();
+      assertThat(foundProject.getProjectUserList().get(1)).isNotNull();
     }
 
     @Test
-    void 팀원_강퇴() throws Exception {
-      User user = testRepository.findMe();
-      Project project = findJoinedProject();
-      project.updateHost(user);
-      projectRepository.save(project);
+    @DisplayName("팀원_강퇴")
+    void kickUser() throws Exception {
+      Project project = projectFixture.joinProject(2L, 1L);
 
       mockMvc.perform(delete(END_POINT)
-          .header("Authorization", idToken)
-          .param("projectUserIdList", String.valueOf(project.getProjectUserList().get(1).getId()))
+          .header("Authorization", token)
+          .param("projectUserIdList",
+              String.valueOf(project.getProjectUserList().get(1).getId()))
       ).andExpect(status().isNoContent());
 
-      List<ProjectUser> projectUserList = projectUserRepository.findAllByProjectId(project.getId());
-      assertThat(projectUserList.size()).isEqualTo(project.getProjectUserList().size() - 1);
+      List<ProjectUser> projectUserList = projectUserRepository.findAllByProjectId(1L);
+      assertThat(projectUserList).hasSize(1);
     }
 
     @Test
-    void 팀플_탈퇴() throws Exception {
-      Project project = findJoinedProject();
+    @DisplayName("팀플_탈퇴")
+    void quitProject() throws Exception {
+      projectFixture.joinProject(1L, 2L);
 
-      mockMvc.perform(delete(END_POINT + "/" + project.getId())
-          .header("Authorization", idToken)
+      mockMvc.perform(delete(END_POINT + "/2")
+          .header("Authorization", token)
       ).andExpect(status().isNoContent());
 
-      List<ProjectUser> projectUserList = projectUserRepository.findAllByProjectId(project.getId());
-      assertThat(projectUserList.size()).isEqualTo(project.getProjectUserList().size() - 1);
+      List<ProjectUser> projectUserList = projectUserRepository.findAllByProjectId(2L);
+      assertThat(projectUserList).hasSize(1);
     }
   }
 
-  private ProjectUser findProjectUserById(Long id) {
-    return projectUserRepository.findById(id).orElseThrow(NoSuchElementException::new);
+  @Nested
+  @DisplayName("실패")
+  class Fail {
+    @Nested
+    @DisplayName("팀원_초대용_hashedId_조회")
+    class ReadHashedId {
+      @Test
+      @DisplayName("프로젝트_없음")
+      void notFound() throws Exception {
+        mockMvc.perform(get(END_POINT + "/111/invite")
+            .header("Authorization", token)
+        ).andExpect(status().isNotFound());
+      }
+    }
+
+    @Nested
+    @DisplayName("팀원_목록_조회")
+    class ReadProjectUsers {
+      @Test
+      @DisplayName("프로젝트_없음")
+      void notFound() throws Exception {
+        mockMvc.perform(get(END_POINT + "/111")
+            .header("Authorization", token)
+        ).andExpect(status().isNotFound());
+      }
+    }
   }
 
-  private Project findJoinedProject() {
-    User user = DataInitializer.testUser;
-    ProjectPaginationParam param = new ProjectPaginationParam(0, 10, false, user.getId(), null, null);
-    List<Project> projectList = projectRepository.findAllByUserIdAndIsDone(param.toPageable(), user.getId(), false).getContent();
-    if (projectList.isEmpty()) {
-      throw new NoSuchElementException("not found project");
+  @Nested
+  @DisplayName("담당_역할_변경")
+  class UpdateRole {
+    @Test
+    @DisplayName("프로젝트_없음")
+    void notFound() throws Exception {
+      mockMvc.perform(patch(END_POINT)
+          .header("Authorization", token)
+          .param("projectId", String.valueOf(2))
+          .param("role", "test role")
+      ).andExpect(status().isNotFound());
     }
-    return projectList.get(0);
+  }
+
+  @Nested
+  @DisplayName("초대_수락")
+  class AcceptInvite {
+    @Test
+    @DisplayName("초대_수락")
+    void acceptInvite() throws Exception {
+      mockMvc.perform(patch(END_POINT + "/test hashedId")
+          .header("Authorization", token)
+      ).andExpect(status().isNotFound());
+    }
+  }
+
+  @Nested
+  @DisplayName("팀원_강퇴")
+  class KickUser {
+    @Test
+    @DisplayName("param_오타_String")
+    void invalidProjectUserId() throws Exception {
+      projectFixture.joinProject(2L, 1L);
+
+      mockMvc.perform(delete(END_POINT)
+          .header("Authorization", token)
+          .param("projectUserIdList", "asd")
+      ).andExpect(status().isInternalServerError());
+    }
   }
 }
