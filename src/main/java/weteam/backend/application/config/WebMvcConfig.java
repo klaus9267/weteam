6f7748e908adbc26ca18339596bf979290d8c6ce
Kim minho <klaus9267@gmail.com>
2025-01-17 12:42:18 +0900

    Fix: cors 이슈 해결

--- src/main/java/weteam/backend/application/config/WebMvcConfig.java
+++ src/main/java/weteam/backend/application/config/WebMvcConfig.java
@@ -12,11 +12,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOriginPatterns("/*")    // 외부에서 들어오는 모둔 url 을 허용
+        .allowedOriginPatterns("*")    // 외부에서 들어오는 모둔 url 을 허용
         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")    // 허용되는 Method
         .allowedHeaders("*")    // 허용되는 헤더
-        .allowCredentials(true)    // 자격증명 허용
-        .maxAge(3600);   // 허용 시간
+        .allowCredentials(true);    // 자격증명 허용
   }
 
   @Override
